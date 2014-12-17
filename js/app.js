6fc37a2279e08005d40173470aa4d8bb2b084e43
bayers92 <brianc.ayers@gmail.com>
2014-12-17 15:52:39 -0500

    landing page and overall styling

--- js/app.js
+++ js/app.js
@@ -4,15 +4,15 @@ $(document).foundation();
 
 
 // Magellan top bar preferences
-// $(document).foundation({
-// "magellan-expedition": {
-//   active_class: 'active', // specify the class used for active sections
-//   threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
-//   destination_threshold: 20, // pixels from the top of destination for it to be considered active
-//   throttle_delay: 0, // calculation throttling to increase framerate
-//   fixed_top: 0, // top distance in pixels assigned to the fixed element on scroll
-// }
-// });
+$(document).foundation({
+"magellan-expedition": {
+  active_class: 'active', // specify the class used for active sections
+  threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
+  destination_threshold: 20, // pixels from the top of destination for it to be considered active
+  throttle_delay: 0, // calculation throttling to increase framerate
+  fixed_top: 0, // top distance in pixels assigned to the fixed element on scroll
+}
+});
 
 
 // offcanvas nav preferences
@@ -25,4 +25,10 @@ $(document).foundation({
     // [ true | false ]
     close_on_click : true
   }
+});
+
+$(".rotate").textrotator({
+  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  speed: 2500 // How many milliseconds until the next word show.
 });
