187874593b62e0b6e933e787ec8c755e46c4fbd6
bayers92 <brianc.ayers@gmail.com>
2014-12-22 10:37:25 -0500

    currently using tumblr js feed

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).foundation({
 "magellan-expedition": {
   active_class: 'active', // specify the class used for active sections
   threshold: 0, // how many pixels until the magellan bar sticks, 0 = auto
-  destination_threshold: 20, // pixels from the top of destination for it to be considered active
+  destination_threshold: 40, // pixels from the top of destination for it to be considered active
   throttle_delay: 0, // calculation throttling to increase framerate
   fixed_top: 0, // top distance in pixels assigned to the fixed element on scroll
 }
